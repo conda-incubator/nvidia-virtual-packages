5a1be4cf855cae92aac7cbab6a07fb0aefb9b3e8
Daniel Ching <dching@nvidia.com>
2025-03-12 13:50:41 -0500

    NEW: Create a conda plugin for the __cuda_arch virtual package

---
+++ nvidia_virtual_packages/__init__.py
@@ -0,0 +1,3 @@
+# Placeholder for poetry-dynamic-versioning, do not change:
+# https://github.com/mtkennerly/poetry-dynamic-versioning#installation
+__version__ = "0.0.0"
---
+++ nvidia_virtual_packages/cuda/arch.py
@@ -0,0 +1,45 @@
+"""Define a virtual package exposing the arch of CUDA devices on the system.
+
+The virtual package will be `__cuda_arch`, and will provide the minimum SM of CUDA devices
+detected on the system. The version will be the SM version and the build string will be the
+device model.
+
+"""
+
+import typing
+
+import functools
+
+from conda import plugins
+import cuda.core.experimental as ccx
+
+
+def get_minimum_sm() -> tuple[str, typing.Union[None, str]]:
+    """Try to detect the minimum SM of CUDA devices on the system."""
+    minimum_sm_major: int = 999
+    minimum_sm_minor: int = 999
+    device_name: str = "None"
+    for device in ccx.system.devices:
+        if (
+            device.compute_capability.major < minimum_sm_major
+            and device.compute_capability.minor < minimum_sm_minor
+        ):
+            minimum_sm_major = device.compute_capability.major
+            minimum_sm_minor = device.compute_capability.minor
+            device_name = device.name
+    stripped_name = device_name.replace(" ", "").replace("NVIDIA", "")
+    return f"{minimum_sm_major}.{minimum_sm_minor}", stripped_name
+
+
+@functools.cache
+def cached_minimum_sm():
+    """Return a cached version of the minimum_sm."""
+    return get_minimum_sm()
+
+
+@plugins.hookimpl
+def conda_virtual_packages():
+    minimum_sm, device_model_name = cached_minimum_sm()
+    yield plugins.CondaVirtualPackage(
+        name="cuda_arch", version=minimum_sm, build=device_model_name
+    )
