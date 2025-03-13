55306ab761070c283679a7b23c7e3a482f349ef7
Daniel Ching <dching@nvidia.com>
2025-03-13 17:52:20 -0500

    NEW: Add demo/test recipe to tests

---
+++ tests/features/build.sh
@@ -0,0 +1,5 @@
+mkdir -p ${PREFIX}/bin
+echo "#! /bin/bash" > ${PREFIX}/bin/hello.sh
+echo "set -ex" >> ${PREFIX}/bin/hello.sh
+echo "echo \"This package was built with CUDAARCHS=${CUDAARCHS}\"" >> ${PREFIX}/bin/hello.sh
+chmod u+x ${PREFIX}/bin/hello.sh
