3bcb8c27ee54e3e904a3ef953a5131dbfd695927
Daniel Ching <dching@nvidia.com>
2025-10-13 16:03:08 -0500

    BUG: Cannot type hint library

--- nvidia_virtual_packages/cuda/arch.py
+++ nvidia_virtual_packages/cuda/arch.py
@@ -19,7 +19,6 @@ import typing
 from conda import plugins
 
 
-library: typing.Union[ctypes.WinDLL, ctypes.CDLL]
 if os.name == "nt":
     library = ctypes.WinDLL(ctypes.util.find_library("nvcuda"))
 elif os.name == "posix":
