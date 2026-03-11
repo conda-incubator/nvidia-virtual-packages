4577c28c888fb9deaec7ee4d3bb2ba879de9a27e
Daniel Ching <dching@nvidia.com>
2026-03-11 13:33:55 -0500

    MAI: Add version information to git archive

--- nvidia_virtual_packages/__init__.py
+++ nvidia_virtual_packages/__init__.py
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Placeholder for poetry-dynamic-versioning, do not change:
-# https://github.com/mtkennerly/poetry-dynamic-versioning#installation
-__version__ = "0.0.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
